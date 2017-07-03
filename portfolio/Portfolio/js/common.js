9e493aed8aa01250f17b5280b288996afbeb46f2
alex-pirat <asquerquet@gmail.com>
2017-07-03 22:04:52 +0300

    portfolio

--- portfolio/Portfolio/js/common.js
+++ portfolio/Portfolio/js/common.js
@@ -1,8 +1,10 @@
 $(function() {
   $('.main_menu').click(function() {
     $('.main_menu_opened').show(500);
+    $('.main_logo').hide(500);
   });
   $('.main_menu_opened .closes').click(function() {
     $('.main_menu_opened').hide(500);
+    $('.main_logo').show(500);
   });
 });
